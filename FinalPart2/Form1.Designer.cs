b3fe5e32a247f8630e6569d0c33ec915b1b315f6
Anthony Barnes <tonylbarnes@gmail.com>
2023-10-03 14:06:35 -0400

    Add project files.

---
+++ FinalPart2/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace FinalPart2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CoeffA1TextBox = new System.Windows.Forms.TextBox();
+            this.equationLabel1 = new System.Windows.Forms.Label();
+            this.xLabel1 = new System.Windows.Forms.Label();
+            this.CoeffB1TextBox = new System.Windows.Forms.TextBox();
+            this.yLabel1 = new System.Windows.Forms.Label();
+            this.CoeffC1TextBox = new System.Windows.Forms.TextBox();
+            this.CoeffC2TextBox = new System.Windows.Forms.TextBox();
+            this.yLabel2 = new System.Windows.Forms.Label();
+            this.CoeffB2TextBox = new System.Windows.Forms.TextBox();
+            this.xLabel2 = new System.Windows.Forms.Label();
+            this.equationLabel2 = new System.Windows.Forms.Label();
+            this.CoeffA2TextBox = new System.Windows.Forms.TextBox();
+            this.import = new System.Windows.Forms.Button();
+            this.solve = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CoeffA1TextBox
+            // 
+            this.CoeffA1TextBox.Location = new System.Drawing.Point(93, 54);
+            this.CoeffA1TextBox.Name = "CoeffA1TextBox";
+            this.CoeffA1TextBox.Size = new System.Drawing.Size(30, 20);
+            this.CoeffA1TextBox.TabIndex = 0;
+            // 
+            // equationLabel1
+            // 
+            this.equationLabel1.AutoSize = true;
+            this.equationLabel1.Location = new System.Drawing.Point(29, 58);
+            this.equationLabel1.Name = "equationLabel1";
+            this.equationLabel1.Size = new System.Drawing.Size(58, 13);
+            this.equationLabel1.TabIndex = 1;
+            this.equationLabel1.Text = "Equation 1";
+            // 
+            // xLabel1
+            // 
+            this.xLabel1.AutoSize = true;
+            this.xLabel1.Location = new System.Drawing.Point(124, 58);
+            this.xLabel1.Name = "xLabel1";
+            this.xLabel1.Size = new System.Drawing.Size(24, 13);
+            this.xLabel1.TabIndex = 2;
+            this.xLabel1.Text = "x + ";
+            // 
+            // CoeffB1TextBox
+            // 
+            this.CoeffB1TextBox.Location = new System.Drawing.Point(145, 54);
+            this.CoeffB1TextBox.Name = "CoeffB1TextBox";
+            this.CoeffB1TextBox.Size = new System.Drawing.Size(30, 20);
+            this.CoeffB1TextBox.TabIndex = 3;
+            // 
+            // yLabel1
+            // 
+            this.yLabel1.AutoSize = true;
+            this.yLabel1.Location = new System.Drawing.Point(178, 58);
+            this.yLabel1.Name = "yLabel1";
+            this.yLabel1.Size = new System.Drawing.Size(24, 13);
+            this.yLabel1.TabIndex = 4;
+            this.yLabel1.Text = "y = ";
+            // 
+            // CoeffC1TextBox
+            // 
+            this.CoeffC1TextBox.Location = new System.Drawing.Point(202, 54);
+            this.CoeffC1TextBox.Name = "CoeffC1TextBox";
+            this.CoeffC1TextBox.Size = new System.Drawing.Size(30, 20);
+            this.CoeffC1TextBox.TabIndex = 5;
+            // 
+            // CoeffC2TextBox
+            // 
+            this.CoeffC2TextBox.Location = new System.Drawing.Point(430, 54);
+            this.CoeffC2TextBox.Name = "CoeffC2TextBox";
+            this.CoeffC2TextBox.Size = new System.Drawing.Size(30, 20);
+            this.CoeffC2TextBox.TabIndex = 11;
+            // 
+            // yLabel2
+            // 
+            this.yLabel2.AutoSize = true;
+            this.yLabel2.Location = new System.Drawing.Point(406, 58);
+            this.yLabel2.Name = "yLabel2";
+            this.yLabel2.Size = new System.Drawing.Size(24, 13);
+            this.yLabel2.TabIndex = 10;
+            this.yLabel2.Text = "y = ";
+            // 
+            // CoeffB2TextBox
+            // 
+            this.CoeffB2TextBox.Location = new System.Drawing.Point(373, 54);
+            this.CoeffB2TextBox.Name = "CoeffB2TextBox";
+            this.CoeffB2TextBox.Size = new System.Drawing.Size(30, 20);
+            this.CoeffB2TextBox.TabIndex = 9;
+            // 
+            // xLabel2
+            // 
+            this.xLabel2.AutoSize = true;
+            this.xLabel2.Location = new System.Drawing.Point(352, 58);
+            this.xLabel2.Name = "xLabel2";
+            this.xLabel2.Size = new System.Drawing.Size(24, 13);
+            this.xLabel2.TabIndex = 8;
+            this.xLabel2.Text = "x + ";
+            // 
+            // equationLabel2
+            // 
+            this.equationLabel2.AutoSize = true;
+            this.equationLabel2.Location = new System.Drawing.Point(257, 58);
+            this.equationLabel2.Name = "equationLabel2";
+            this.equationLabel2.Size = new System.Drawing.Size(58, 13);
+            this.equationLabel2.TabIndex = 7;
+            this.equationLabel2.Text = "Equation 2";
+            // 
+            // CoeffA2TextBox
+            // 
+            this.CoeffA2TextBox.Location = new System.Drawing.Point(321, 54);
+            this.CoeffA2TextBox.Name = "CoeffA2TextBox";
+            this.CoeffA2TextBox.Size = new System.Drawing.Size(30, 20);
+            this.CoeffA2TextBox.TabIndex = 6;
+            // 
+            // import
+            // 
+            this.import.Location = new System.Drawing.Point(16, 268);
+            this.import.Name = "import";
+            this.import.Size = new System.Drawing.Size(75, 23);
+            this.import.TabIndex = 12;
+            this.import.Text = "Import";
+            this.import.UseVisualStyleBackColor = true;
+            this.import.Click += new System.EventHandler(this.Import_Click);
+            // 
+            // solve
+            // 
+            this.solve.Location = new System.Drawing.Point(136, 268);
+            this.solve.Name = "solve";
+            this.solve.Size = new System.Drawing.Size(240, 23);
+            this.solve.TabIndex = 13;
+            this.solve.Text = "Solve";
+            this.solve.UseVisualStyleBackColor = true;
+            this.solve.Click += new System.EventHandler(this.Solve_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 31);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Input";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(29, 134);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(223, 13);
+            this.ResultLabel.TabIndex = 20;
+            this.ResultLabel.Text = "Please enter your coefficients or import a file...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 31);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Output";
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(409, 268);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 21;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 313);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.solve);
+            this.Controls.Add(this.import);
+            this.Controls.Add(this.CoeffC2TextBox);
+            this.Controls.Add(this.yLabel2);
+            this.Controls.Add(this.CoeffB2TextBox);
+            this.Controls.Add(this.xLabel2);
+            this.Controls.Add(this.equationLabel2);
+            this.Controls.Add(this.CoeffA2TextBox);
+            this.Controls.Add(this.CoeffC1TextBox);
+            this.Controls.Add(this.yLabel1);
+            this.Controls.Add(this.CoeffB1TextBox);
+            this.Controls.Add(this.xLabel1);
+            this.Controls.Add(this.equationLabel1);
+            this.Controls.Add(this.CoeffA1TextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Linear Equation Solver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CoeffA1TextBox;
+        private System.Windows.Forms.Label equationLabel1;
+        private System.Windows.Forms.Label xLabel1;
+        private System.Windows.Forms.TextBox CoeffB1TextBox;
+        private System.Windows.Forms.Label yLabel1;
+        private System.Windows.Forms.TextBox CoeffC1TextBox;
+        private System.Windows.Forms.TextBox CoeffC2TextBox;
+        private System.Windows.Forms.Label yLabel2;
+        private System.Windows.Forms.TextBox CoeffB2TextBox;
+        private System.Windows.Forms.Label xLabel2;
+        private System.Windows.Forms.Label equationLabel2;
+        private System.Windows.Forms.TextBox CoeffA2TextBox;
+        private System.Windows.Forms.Button import;
+        private System.Windows.Forms.Button solve;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ FinalPart2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalPart2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Import_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                // Set filter for text or all files
+                Filter = "Text Files|*.txt|All Files|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+
+                try
+                {
+                    // Read coefficients from the selected text file
+                    string[] lines = File.ReadAllLines(fileName);
+
+                    // Make sure file has 2 lines
+                    if (lines.Length >= 2)
+                    {
+                        // TryParse coefficients from string to pass back to TextBoxes
+                        if (TryParseCoefficientsFromString(lines[0], out double a1, out double b1, out double c1) &&
+                            TryParseCoefficientsFromString(lines[1], out double a2, out double b2, out double c2))
+                        {
+                            CoeffA1TextBox.Text = a1.ToString();
+                            CoeffB1TextBox.Text = b1.ToString();
+                            CoeffC1TextBox.Text = c1.ToString();
+                            CoeffA2TextBox.Text = a2.ToString();
+                            CoeffB2TextBox.Text = b2.ToString();
+                            CoeffC2TextBox.Text = c2.ToString();
+
+                            ResultLabel.ForeColor = Color.Green;
+                            ResultLabel.Text = $"Imported coefficients from file {fileName}.";
+
+                        }
+                        else
+                        {
+                            // Let user know data in file is invalid
+                            ResultLabel.ForeColor = Color.Red;
+                            ResultLabel.Text = "Invalid data format in the file.";
+                        }
+                    }
+                    else
+                    {
+                        // Let user know file must have at least 2 lines
+                        ResultLabel.ForeColor = Color.Red;
+                        ResultLabel.Text = "The file must contain at least two lines of coefficients.";
+                    }
+                }
+                catch (IOException)
+                {
+                    // Catch error if file can't be read for some reason
+                    ResultLabel.ForeColor = Color.Red;
+                    ResultLabel.Text = "Error reading the file.";
+                }
+            }
+        }
+
+        private bool TryParseCoefficients(out double a1, out double b1, out double c1, out double a2, out double b2, out double c2)
+        {
+            // Initialize parameters
+            a1 = b1 = c1 = a2 = b2 = c2 = 0.0;
+
+            // Try parsing all of the values in the text boxes, returning the parsed values for each coefficient and a boolean value to show success/failure 
+            if (double.TryParse(CoeffA1TextBox.Text, out a1) &&
+                double.TryParse(CoeffB1TextBox.Text, out b1) &&
+                double.TryParse(CoeffC1TextBox.Text, out c1) &&
+                double.TryParse(CoeffA2TextBox.Text, out a2) &&
+                double.TryParse(CoeffB2TextBox.Text, out b2) &&
+                double.TryParse(CoeffC2TextBox.Text, out c2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool TryParseCoefficientsFromString(string line, out double a, out double b, out double c)
+        {
+            // Initialize parameters
+            a = b = c = 0.0; 
+
+            // Split the input line by both commas and spaces
+            string[] delimiters = { ",", " " };
+            string[] parts = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length == 3)
+            {
+                // Try to parse the coefficients from the split parts
+                if (double.TryParse(parts[0], out a) &&
+                    double.TryParse(parts[1], out b) &&
+                    double.TryParse(parts[2], out c))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void Solve_Click(object sender, EventArgs e)
+        {
+            // Parse coefficients from text boxes
+            if (TryParseCoefficients(out double a1, out double b1, out double c1, out double a2, out double b2, out double c2))
+            {
+                // Check if the lines are parallel or identical
+                if (a1 * b2 == a2 * b1 && b1 * c2 == b2 * c1)
+                {
+                    ResultLabel.ForeColor = Color.Black;
+                    ResultLabel.Text = "The two lines are identical.";
+                }
+                else if (a1 * b2 == a2 * b1)
+                {
+                    ResultLabel.ForeColor = Color.Black;
+                    ResultLabel.Text = "The two lines are parallel and do not intersect.";
+                }
+                else
+                {
+                    // Calculate the intersection point
+                    double x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+                    double y = (c1 * a2 - c2 * a1) / (b1 * a2 - b2 * a1);
+                    ResultLabel.ForeColor = Color.Green;
+                    ResultLabel.Text = $"The intersection point is at ({x}, {y}).";
+                }
+            }
+            else
+            {
+                ResultLabel.ForeColor = Color.Red;
+                ResultLabel.Text = "Invalid input. Please enter valid coefficients.";
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            CoeffA1TextBox.Text = CoeffB1TextBox.Text = CoeffC1TextBox.Text = string.Empty;
+            CoeffA2TextBox.Text = CoeffB2TextBox.Text = CoeffC2TextBox.Text = string.Empty;
+            ResultLabel.ForeColor = Color.Black;
+            ResultLabel.Text = string.Empty;
+        }
+    }
+}
